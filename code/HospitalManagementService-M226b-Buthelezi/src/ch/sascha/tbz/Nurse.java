5dcd30f6019d5bee11c4ffd720579ea7af4da472
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2022-01-25 11:23:01 +0100

    add workers.

---
+++ code/HospitalManagementService-M226b-Buthelezi/src/ch/sascha/tbz/Nurse.java
@@ -0,0 +1,6 @@
+package ch.sascha.tbz;
+
+import ch.sascha.tbz.abstracts.StaffMember;
+
+public class Nurse extends StaffMember {
+}
---
+++ code/HospitalManagementService-M226b-Buthelezi/src/ch/sascha/tbz/abstracts/StaffMember.java
@@ -0,0 +1,14 @@
+package ch.sascha.tbz.abstracts;
+
+import java.time.LocalDate;
+
+public abstract class StaffMember {
+
+    public String staffNumber;
+    public LocalDate startDate;
+
+    public StaffMember(){
+    }
+
+
+}
